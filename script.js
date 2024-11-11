7a68ffebb34c9c3981aeccb045340977bb74d651
PedroArmasHoops <pedroarmas233@gmail.com>
2024-11-11 10:45:43 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Archivo script.js
+document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Formulario enviado. Nos pondremos en contacto contigo pronto.");
+});
